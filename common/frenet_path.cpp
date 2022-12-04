2ce4569546dfe5d211b1c6592e4ba547f9934a33
chong.yin <yinchong2014@outlook.com>
2022-12-04 22:40:10 +0800

    update

---
+++ common/frenet_path.cpp
@@ -0,0 +1,15 @@
+#include "frenet_path.h"
+
+int findGlobalNearestPoint() {
+    //
+}
+
+int findLocalNearestPoint() {
+    //
+}
+
+
+int findNearestPoint() {
+    static bool is_first_run = false;
+    static int pre_nearest_point = 0;
+}
